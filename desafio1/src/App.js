720283683537c4004cee24f52d98668537b51e36
catitorres <mariacatalinatorres123@gmail.com>
2021-11-01 20:32:38 +0000

    bootstrap added

--- desafio1/src/App.js
+++ desafio1/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Inicio <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Productos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Sobre nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
