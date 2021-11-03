6b3bfefc706db4c204e9ec1f117fe73a2fd4e66f
catitorres <mariacatalinatorres123@gmail.com>
2021-11-03 14:42:29 +0000

    cambio de nombre raiz

---
+++ eCommerce/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from "./components/navBar";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
